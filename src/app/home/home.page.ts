9f1bc66d0ea3f9ff637eb733f70e607b5254b3c6
manoelsouzadev <manoel.souza280@gmail.com>
2020-02-18 21:25:03 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { CameraOptions, Camera } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  photo: string = '';
 
-  constructor() {}
+  constructor(private camera: Camera) {}
 
+  takePicture() {
+    this.photo = '';
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      allowEdit: true
+    };
+
+    this.camera
+      .getPicture(options)
+      .then(
+        ImageData => {
+          let base64Image = 'data:image/jpeg;base64,' + ImageData;
+          this.photo = base64Image;
+        },
+        err => {
+          console.log(err);
+        }
+      )
+      .catch(error => {
+        console.log(error);
+      });
+  }
 }
